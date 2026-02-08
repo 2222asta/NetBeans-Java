e13963466a1ce02247072d701df7892b373bf694
danie <danie@DESKTOP-JS62ABA>
2026-02-07 18:09:31 -0600

    Subida inicial java

---
+++ src/tema1ide/HelloWord.java
@@ -0,0 +1,24 @@
+
+/*
+*2026 ITESS.TICS
+*Periodo: Enero-Junio
+*Asignatura: Programacion Orientada a Objetos 
+*Docente: Francisco Javier Montecillo Puente
+*Descripcion: Tema1. Entorno de desarrollo 
+                     Prueba del entorno de desarrollo y github
+*Programador: ADEZ
+*Fecha: o6 febrero 2026
+*/
+
+package tema1ide;
+
+/**
+ *
+ * @author daniel
+ */
+public class HelloWord {
+    public void sayHello(String name){
+        System.out.println("hello, "+name);
+        
+    }
+}
---
+++ src/tema1ide/HelloWorldApp.java
@@ -0,0 +1,24 @@
+/*
+*2026 ITESS.TICS
+*Periodo: Enero-Junio
+*Asignatura: Programacion Orientada a Objetos 
+*Docente: Francisco Javier Montecillo Puente
+*Descripcion: Tema1. Entorno de desarrollo 
+                     Prueba del entorno de desarrollo y github
+*Programador: ADEZ
+*Fecha: o6 febrero 2026
+*/
+package tema1ide;
+
+/**
+ *
+ * @author danie
+ */
+public class HelloWorldApp {
+    public static void main(String [] args){
+        HelloWord hello = new HelloWord();
+        
+        hello.sayHello("ADEZ");
+    }
+    
+}
